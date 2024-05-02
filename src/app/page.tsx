2314e69547fe72c2557902a06121a48cfa0c998c
Brenno-Savio <brennosavio@hotmail.com>
2024-05-02 19:11:13 -0300

    fixing redirection login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@
 
 import Tasks from '@/components/tasks/Tasks';
 import { useGlobalState } from '@/context/globalProvider';
+import { redirect } from 'next/navigation';
 
 const Home = () => {
   const { tasks } = useGlobalState();
+
+  if(tasks.length <= 0) {
+    redirect('/signin');
+  }
 
   return <Tasks title="All Tasks" tasks={tasks} />;
 };
